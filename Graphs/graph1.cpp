847bf9899f2f77c2854566fb492c995e16ff60c7
karinasharma01 <68894444+karinasharma01@users.noreply.github.com>
2020-08-24 17:58:48 +0530

    Updated

--- Graphs/graph1.cpp
+++ Graphs/graph1.cpp
@@ -35,6 +35,49 @@ void DFS(int** edges,int n)
     }
     delete [] visited;
 }
+//Breadth First Search
+void printBFS(int** edges,int n,int startV,bool* visited){
+queue<int> pendingVertices;
+pendingVertices.push(startV);
+    visited[startV]=true;
+while(!pendingVertices.empty())
+{
+    int currentVertex=pendingVertices.front();
+    pendingVertices.pop();
+    cout<<currentVertex<<endl;
+    for(int i=0;i<n;i++)
+    {
+        if(i==currentVertex)
+        {
+            continue;
+        }
+
+    if(edges[currentVertex][i]==1 && !visited[i])
+    {
+        pendingVertices.push(i);
+        visited[i]=true;
+    }
+    }
+}
+
+}
+void BFS(int** edges,int n)
+{
+    bool* visited=new bool[n];
+    for(int i=0;i<n;i++)
+    {
+        visited[i]=false;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(visited[i]==false)//ie the vertex or node hasn't been visited then that vertex becomes the startVertex for that call
+        {
+            printBFS(edges,n,i,visited);
+        }
+    }
+    delete [] visited;
+
+}
 int main()
 {//the graph is being implemented using an Adjacency Matrix
 int n,e;//n is the no of vertices in graph numbered from 0 to n-1
