1abd7aeea6f5d65d1a7973b8409c39bf5a422448
karinasharma01 <68894444+karinasharma01@users.noreply.github.com>
2021-05-21 15:02:28 +0530

    heaps | priority queue

---
+++ LeetCode/Easy/Last Stone Weight.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();//number of stones
+        priority_queue<int> pq;
+        for(int i=0;i<n;i++)
+        {
+            pq.push(stones[i]);
+        }
+        while(pq.size()>1)
+        {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            if(x>y)
+            {
+                pq.push(x-y);
+            }
+        }
+        if(pq.size()==0)
+            return 0;
+        return pq.top();
+    }
+};
