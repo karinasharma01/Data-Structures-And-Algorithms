0ad7e69ce3cf1f5c8c1da095859d6725c969afbe
karinasharma01 <68894444+karinasharma01@users.noreply.github.com>
2021-05-28 20:57:35 +0530

    binary search | array | hashtable | two pointers

--- LeetCode/Easy/Intersection of Two Arrays.cpp
+++ LeetCode/Easy/Intersection of Two Arrays.cpp
@@ -40,3 +40,32 @@ public:
         return res;
     }
 };
+
+//Method-2:Using hashsets
+//Time Complexity:O(n)
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> s1;
+        set<int> s2;
+        for(int i=0;i<nums1.size();i++)
+        {
+            s1.insert(nums1[i]);
+        }
+        for(int i=0;i<nums2.size();i++)
+        {
+            if(s1.find(nums2[i])!=s1.end())
+            {
+                s2.insert(nums2[i]);
+            }
+        }
+        int i = 0;
+        vector<int> result(s2.size());
+        for(int num:s2)
+        {
+            result[i++]=num;
+        }
+        return result;
+    }
+};
