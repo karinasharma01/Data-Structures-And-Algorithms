e3e06301f274e49cb12251371c3460312ffee7e1
karinasharma01 <68894444+karinasharma01@users.noreply.github.com>
2021-06-01 21:03:02 +0530

    sliding window | two pointers

---
+++ LeetCode/Medium/Longest Repeating Character Replacement.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findLength(string s,int k,char ch)
+    {
+        int maxLen = 1;
+        int n = s.size();
+        int i=0,j=0;
+        int count = 0;
+        while(j < n)
+        {
+            if(s[j]!=ch)
+                count++;
+            while(count>k)
+            {
+                if(s[i]!=ch)
+                    count--;
+                i++;
+            }
+            maxLen=max(maxLen,j-i+1);
+            j++;
+        }
+        return maxLen;
+    }
+    int characterReplacement(string s, int k) {
+        /*we need to find the maximum length of the substring that can be formed by each character
+        and  whichever character will form the sub-string of maximum length then that length will          be our output*/
+        int maxLen = 1;
+        for(int i=0;i<26;i++)
+        {
+            maxLen = max(maxLen,findLength(s,k,i+'A'));
+        }
+        return maxLen;
+    }
+};
